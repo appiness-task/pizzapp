e419e631bc3f3ac5726b78460a13b6e4c2b4a35c
veera <veera@deliforce.io>
2020-11-05 10:50:35 +0530

    changes commit

--- src/app/_services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './startup.service';
-export * from './http.service';
-export * from './cognito.service';
-export * from './aws.service';
-export * from './auth.service';
-export * from './notification.service';
-export * from './country-name.service';
